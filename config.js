8e231a442fb792dc3bca9b36d396cca60efb678a
Nandini Gnanesh <nandini.gnanesh@exathought.com>
2022-07-18 11:39:40 +0530

    code our GET programming languages API with pagination.

---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {
+    db: {
+      /* don't expose password or any sensitive info, done only for demo */
+      host: "db4free.net",
+      user: "user",
+      password: "pwd",
+      database: "db",
+    },
+    listPerPage: 10,
+  };
+  module.exports = config;
